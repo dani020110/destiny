37ec1882f296ce3ed9b179b001b203e9326a7aba
Daniel Vásquez <danielgusvt@yahoo.com>
2017-01-16 18:14:59 -0500

    Oops

--- build.sh
+++ build.sh
@@ -11,26 +11,26 @@ NCOLOR='\033[0m'
 export CROSS_COMPILE=/media/DATOS/desarrollo/kernel/toolchain/ubertc_kernel/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
 export out=/media/DATOS/desarrollo/output/kernel
 
-#echo -e "${WHITE}Cleaning up${NCOLOR}"
-#make O=${out} mrproper
-#rm ${out}/*.img
+echo -e "${WHITE}Cleaning up${NCOLOR}"
+make O=${out} mrproper
+rm ${out}/*.img
 #To do: find a workaround for the .dtb's not being deleted
-#rm $(find ${out} -name '*.dtb')
-#echo ""
+rm $(find ${out} -name '*.dtb')
+echo ""
 
-#echo -e "${PINK}Compiling for tulip${NCOLOR}"
-#make ARCH=arm64 O=${out} CROSS_COMPILE=$CROSS_COMPILE kanuti-tulip_defconfig
-#echo ""
+echo -e "${PINK}Compiling for tulip${NCOLOR}"
+make ARCH=arm64 O=${out} CROSS_COMPILE=$CROSS_COMPILE kanuti-tulip_defconfig
+echo ""
 
-#echo -e "${BLUE}Let's start the kernel compilation${NCOLOR}"
-#echo ""
-#time make ARCH=arm64 O=${out} CROSS_COMPILE=$CROSS_COMPILE -j$(grep -c ^processor /proc/cpuinfo)
-#echo ""
+echo -e "${BLUE}Let's start the kernel compilation${NCOLOR}"
+echo ""
+time make ARCH=arm64 O=${out} CROSS_COMPILE=$CROSS_COMPILE -j$(grep -c ^processor /proc/cpuinfo)
+echo ""
 
-#echo -e "${YELLOW}Making dtb.img${NCOLOR}"
-#echo ""
-#../build_tools/dtbToolCM -2 -o ${out}/dtb.img -s 2048 -p ${out}/scripts/dtc/ ${out}/arch/arm64/boot/dts/
-#echo ""
+echo -e "${YELLOW}Making dtb.img${NCOLOR}"
+echo ""
+../build_tools/dtbToolCM -2 -o ${out}/dtb.img -s 2048 -p ${out}/scripts/dtc/ ${out}/arch/arm64/boot/dts/
+echo ""
 
 echo -e "${LGREEN}Making the boot image for all the variants${NCOLOR}"
 ../build_tools/mkbootimg --kernel ${out}/arch/arm64/boot/Image --ramdisk ../build_tools/ramdisks/e2306-ramdisk.cpio.gz --dt ${out}/dtb.img --base 0x81dfff00 --ramdisk_offset 0x82000000 --tags_offset 0x81E00000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk" -o ${out}/foykernel_${TIMESTAMP}_e2306.img

67c22aa4f05073b9daf2d30efd9e0e91d313de15
Daniel Vásquez <danielgusvt@yahoo.com>
2017-01-18 00:14:34 -0500

    Foy_kernel release 2

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ YELLOW='\033[1;33m'
 NCOLOR='\033[0m'
 
 export CROSS_COMPILE=/media/DATOS/desarrollo/kernel/toolchain/ubertc_kernel/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
+#export CROSS_COMPILE=/media/DATOS/desarrollo/kernel/toolchain/linaro-4.9_aarch64-kernel/bin/aarch64-linux-gnu-
 
 echo -e "${WHITE}Cleaning up${NCOLOR}"
 make mrproper
@@ -26,7 +27,7 @@ echo ""
 time make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE -j$(grep -c ^processor /proc/cpuinfo)
 echo ""
 
-#echo -e "${LGREEN}Making the boot image for all the variants${NCOLOR}"
+echo -e "${GREEN}Making the boot image for all the variants${NCOLOR}"
 ../build_tools/bootimg mkimg --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk" --base 0x81dfff00 --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../build_tools/ramdisks/e2306-ramdisk.cpio.gz --ramdisk_offset 0x82000000 --pagesize 2048 --tags_offset 0x81E00000 -o foykernel_${TIMESTAMP}_e2306.img
 
 ../build_tools/bootimg mkimg --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk" --base 0x81dfff00 --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../build_tools/ramdisks/e2303-ramdisk.cpio.gz --ramdisk_offset 0x82000000 --pagesize 2048 --tags_offset 0x81E00000 -o foykernel_${TIMESTAMP}_e2303.img
@@ -37,4 +38,4 @@ echo ""
 
 ../build_tools/bootimg mkimg --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk" --base 0x81dfff00 --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../build_tools/ramdisks/e2353-ramdisk.cpio.gz --ramdisk_offset 0x82000000 --pagesize 2048 --tags_offset 0x81E00000 -o foykernel_${TIMESTAMP}_e2353.img
 
-echo -e "${GREEN}The kernel has been built successfully${NCOLOR}"
+echo -e "${LGREEN}The kernel has been built successfully${NCOLOR}"

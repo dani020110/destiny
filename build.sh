896f98287636d5ccbbda2e50b585e62c6ca0c658
Daniel Vasquez <danielgusvt@yahoo.com>
2017-02-28 15:51:56 -0500

    Updates build script: update defconfig: diable sysvipc

--- build.sh
+++ build.sh
@@ -12,8 +12,7 @@
 # GNU General Public License for more details.
 #
 
-BUILD_START=$(date +"%s")
-
+BCYAN='\033[1;36m'
 LGREEN='\033[1;32m'
 GREEN='\033[0;32m'
 WHITE='\033[1;37m'
@@ -21,6 +20,8 @@ PINK='\033[1;35m'
 BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
 NCOLOR='\033[0m'
+
+echo -e "${BCYAN}You are building the destiny kernel!${NCOLOR}"
 
 export CROSS_COMPILE=/media/DATOS/desarrollo/kernel/toolchain/ubertc_kernel/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
 #export CROSS_COMPILE=/media/DATOS/desarrollo/kernel/toolchain/linaro-4.9_aarch64-kernel/bin/aarch64-linux-gnu-
@@ -31,6 +32,8 @@ rm *.img
 #To do: find a workaround for the .dtb's not being deleted
 rm $(find -name '*.dtb')
 echo ""
+
+BUILD_START=$(date +"%s")
 
 echo -e "${PINK}Compiling for tulip${NCOLOR}"
 make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE kanuti-tulip_defconfig

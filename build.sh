effae8228c59e370050548f3af96d3a30f3a37af
Daniel Vasquez <danielgusvt@yahoo.com>
2017-03-03 22:20:45 -0500

    Almost ready for the third release Enhance build script Update defconfig

--- build.sh
+++ build.sh
@@ -20,8 +20,9 @@ PINK='\033[1;35m'
 BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
 NCOLOR='\033[0m'
+BOOTIMAGE='../build_tools/destiny-r3.img'
 
-echo -e "${BCYAN}You are building the destiny kernel!${NCOLOR}"
+echo -e "${BCYAN}Hi $USER, you are building the destiny kernel!${NCOLOR}"
 
 export CROSS_COMPILE=/media/DATOS/desarrollo/kernel/toolchain/ubertc_kernel/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
 #export CROSS_COMPILE=/media/DATOS/desarrollo/kernel/toolchain/linaro-4.9_aarch64-kernel/bin/aarch64-linux-gnu-
@@ -29,6 +30,7 @@ export CROSS_COMPILE=/media/DATOS/desarrollo/kernel/toolchain/ubertc_kernel/aarc
 echo -e "${WHITE}Cleaning up${NCOLOR}"
 make mrproper
 rm *.img
+rm $BOOTIMAGE
 #To do: find a workaround for the .dtb's not being deleted
 rm $(find -name '*.dtb')
 echo ""
@@ -44,8 +46,17 @@ echo ""
 make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE -j$(grep -c ^processor /proc/cpuinfo)
 echo ""
 
+#Hmm...
+echo -e "${YELLOW}Making dtb.img${NCOLOR}"
+echo ""
+../build_tools/dtbToolCM -2 -o ../dtb.img -s 2048 -p scripts/dtc/ arch/arm/boot/dts/
+echo ""
+
 echo -e "${GREEN}Making the boot image${NCOLOR}"
-../build_tools/bootimg mkimg --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk" --base 0x81dfff00 --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../build_tools/ramdisks/e2306-ramdisk.cpio.gz --ramdisk_offset 0x82000000 --pagesize 2048 --tags_offset 0x81E00000 -o ../build_tools/destiny-r3.img
+#../build_tools/bootimg mkimg --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk" --base 0x81dfff00 --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../build_tools/ramdisks/e2306-ramdisk.cpio.gz --ramdisk_offset 0x82000000 --pagesize 2048 --tags_offset 0x81E00000 -o $BOOTIMAGE
+
+#Hmm...
+../build_tools/mkbootimg --kernel arch/arm64/boot/Image --ramdisk ../build_tools/ramdisks/e2306-ramdisk.cpio.gz --dt ../dtb.img --base 0x81dfff00 --ramdisk_offset 0x82000000 --tags_offset 0x81E00000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk" -o $BOOTIMAGE
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))

b0ee3bc0c4f5ea71470efe479099347044d6e937
Daniel Vasquez <danielgusvt@yahoo.com>
2017-09-20 21:34:58 -0500

    try to fix ecryptfs

--- kernel/module.c
+++ kernel/module.c
@@ -1181,7 +1181,7 @@ static int check_version(Elf_Shdr *sechdrs,
 		return 1;
 
 	if(!strncmp("ecryptfs", mod->name, 8))
-		return 1;
+		return 0;
 
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)

b381aecaf2f69fd5231d4e642e90677b31bef882
Daniel Vasquez <danielgusvt@yahoo.com>
2018-11-10 22:42:45 -0500

    input: touchscreen: cyttsp4: Move headers to linux/input
    This is more correct because it follows the mainline style.
    
    Signed-off-by: Daniel Vasquez <danielgusvt@yahoo.com>

--- drivers/input/touchscreen/cyttsp4/cyttsp4_debug.c
+++ drivers/input/touchscreen/cyttsp4/cyttsp4_debug.c
@@ -30,8 +30,8 @@
  *
  */
 
-#include <linux/cyttsp4_bus.h>
-#include <linux/cyttsp4_core.h>
+#include <linux/input/cyttsp4_bus.h>
+#include <linux/input/cyttsp4_core.h>
 
 #include <linux/delay.h>
 #include <linux/gpio.h>
